80d84c680a99cfc6b6485c39807de66798e195ec
Andrew Kreps <andrew.kreps@gmail.com>
2014-07-25 00:29:42 -0700

    Constants file

--- lib/cinch/plugins/forecast_io.rb
+++ lib/cinch/plugins/forecast_io.rb
@@ -52,36 +52,4 @@ Once you specify a location, it will persist as long as you own your nick.
 !rain       Yesno?
   EOF
 
-  # Twillio response block
-  get '/forecast' do
-    bot = self.bot
-    request, query = params[:Body].split ' '
-
-    query = get_personalized_query('twillio text', 'weather', query)
-    forecast = bot.plugins[4].get_forecast_io_results query
-
-    case request
-      when /^forecast/i
-        text = bot.plugins[4].get_weather_forecast(request.sub /^forecast\s*/i, '')
-      when /^rain/i
-        text = bot.plugins[4].sms_rain_forecast request.sub(/^rain\s*/i, '')
-      when /^temp/i
-        text = bot.plugins[4].do_the_temp_thing(forecast, %w[_ ▁ ▃ ▅ ▇ █], 24)
-      when /^cond/i
-        text = bot.plugins[4].conditions(forecast)
-      when /^say/i
-        text = request.sub /^say /i, ''
-        bot.Channel('#pdxtech').send(text)
-      when /^wifi/i
-        text = request.sub /^wifi /i, ''
-        bot.Channel('#pdxtech').send("what is #{text} wifi password?")
-      else
-        text = bot.plugins[4].get_weather_forecast(request)
-    end
-
-    twiml = Twilio::TwiML::Response.new do |r|
-      r.Message text
-    end
-    twiml.text
-  end
 end
